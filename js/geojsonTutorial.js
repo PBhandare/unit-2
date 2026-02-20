2e8924420ad402cd728c4e57d1e92af9b13bb24c
PBhandare <40495565+PBhandare@users.noreply.github.com>
2026-02-19 23:49:21 -0600

    Added Comments

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,3 +1,4 @@
+// Create a map centered over the western United States at zoom level 5
 var map = L.map('map').setView([40, -105], 5);
 
 //add tile layer
@@ -6,17 +7,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Define GeoJSON point features
 var geojsonFeature = [{
-    "type": "Feature",
+    "type": "Feature", // GeoJSON feature object
     "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!",
-        "show_on_map": true
+        "name": "Coors Field", // Feature name
+        "amenity": "Baseball Stadium", // Type of amenity
+        "popupContent": "This is where the Rockies play!", // Popup text
+        "show_on_map": true // Boolean for filtering
     },
     "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
+        "type": "Point", // Geometry type
+        "coordinates": [-104.99404, 39.75621] // [longitude, latitude]
     }
 }, {
     "type": "Feature",
@@ -24,7 +26,7 @@ var geojsonFeature = [{
         "name": "Busch Field",
         "amenity": "Sports Stadium",
         "popupContent": "Not named after President Bush?",
-        "show_on_map": false
+        "show_on_map": false // Will be filtered out
     },
     "geometry": {
         "type": "Point",
@@ -32,19 +34,21 @@ var geojsonFeature = [{
     }
 }];
 
+// Define GeoJSON LineString geometries
 var myLines = [{
     "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]] // Array of [lng, lat] pairs
 }, {
     "type": "LineString",
     "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
 }];
 
+// Define GeoJSON polygon features representing states
 var states = [{
     "type": "Feature",
-    "properties": {"party": "Republican"},
+    "properties": {"party": "Republican"}, // Attribute for styling
     "geometry": {
-        "type": "Polygon",
+        "type": "Polygon", // Polygon geometry
         "coordinates": [[
             [-104.05, 48.99],
             [-97.22,  48.98],
@@ -68,12 +72,14 @@ var states = [{
     }
 }];
 
+// Define style for line features
 var myStyle = {
     "color": "#ff7800",
     "weight": 5,
     "opacity": 0.65
 };
 
+// Define styling options for circle markers
 var geojsonMarkerOptions = {
     radius: 8,
     fillColor: "#ff7800",
@@ -83,17 +89,20 @@ var geojsonMarkerOptions = {
     fillOpacity: 0.8
 };
 
+// Function to bind popup if popupContent property exists
 function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
     if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
+        layer.bindPopup(feature.properties.popupContent); // Attach popup
     }
 }
 
+// Add LineString features to map with predefined style
 L.geoJSON(myLines, {
     style: myStyle
 }).addTo(map);
 
+// Add state polygons with conditional styling based on party attribute
 L.geoJSON(states, {
     style: function(feature) {
         switch (feature.properties.party) {
@@ -103,12 +112,14 @@ L.geoJSON(states, {
     }
 }).addTo(map);
 
+// Add point features with filtering, custom marker styling, and popups
 L.geoJSON(geojsonFeature, {
     filter: function(feature) {
-        return feature.properties.show_on_map;
+        return feature.properties.show_on_map; // Only display if true
     },
     pointToLayer: function (feature, latlng) {
+        // Convert points to styled circle markers
         return L.circleMarker(latlng, geojsonMarkerOptions);
     },
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature // Attach popups
 }).addTo(map);
--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -1,5 +1,6 @@
 /* Example from Leaflet Quick Start Guide*/
 
+// Create a Leaflet map centered on London with zoom level 13
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 //add tile layer
@@ -8,36 +9,51 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Add a marker at specified coordinates
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
+// Add a styled circle with 500 meter radius
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,    radius: 500
 }).addTo(map);
 
+// Add a polygon defined by three coordinate vertices
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
 
-marker.bindPopup("<strong>Hello world!</strong><br />I am a popup.").openPopup();
+// Bind and immediately open popup on marker
+marker.bindPopup(
+    "<strong>Hello world!</strong><br />I am a popup."
+).openPopup();
+
+// Bind popup to circle (opens when clicked)
 circle.bindPopup("I am a circle.");
+
+// Bind popup to polygon (opens when clicked)
 polygon.bindPopup("I am a polygon.");
 
+// Create and immediately display a standalone popup
 var popup = L.popup()
     .setLatLng([51.5, -0.09])
     .setContent("I am a standalone popup.")
     .openOn(map);
 
+// Create reusable popup instance
 var popup = L.popup();
 
+// Define function to handle map click events
 function onMapClick(e) {
     popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+        .setLatLng(e.latlng) // Set popup at clicked location
+        .setContent("You clicked the map at " + 
+            e.latlng.toString()) // Display clicked coordinates
+        .openOn(map); // Open popup on map
 }
 
+// Attach click event listener to the map
 map.on('click', onMapClick);
